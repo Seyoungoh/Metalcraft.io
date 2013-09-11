2c47e2d08bd40d664fc56a3f1c7c9faf7a049a79
Chang Ju Park <metalcraft.changju@gmail.com>
2013-09-12 02:18:40 +0900

    주소 연결 변경, change routes index, about, signin, signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Metalcraft::Application.routes.draw do
-  get "static_pages/index"
-  get "static_pages/about"
-  get "static_pages/signin"
-  get "static_pages/signup"
+  match "about"   => "static_pages#about",    via: :get
+  match "signin"  => "static_pages#signin",   via: :get
+  match "signup"  => "static_pages#signup",   via: :get
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'static_pages#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
