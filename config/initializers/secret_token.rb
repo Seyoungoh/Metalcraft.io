0e9485f98db90feb9398983ee674b0f27308bc6f
Chang Ju Park <metalcraft.changju@gmail.com>
2013-09-12 01:14:50 +0900

    Add Readme

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Metalcraft::Application.config.secret_key_base = 'f5912ef8a0fe2eece0c7dd24ec5597eac7f0c006a88e26a49c211a14f1714ec7b950f8f4f1d137102640e2d4e711cb30b572d9e32e8246a38616e279ddd0080b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Metalcraft::Application.config.session_store :cookie_store, key: '_metalcraft_session'
